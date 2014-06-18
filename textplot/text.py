be3682022ea8ef01ba38be4d88d8dc3297977986
David McClure <davidwilliammcclure@gmail.com>
2014-06-18 11:34:20 -0700

    Initial commit.

---
+++ textplot/text.py
@@ -0,0 +1,68 @@
+
+
+from nltk.stem import PorterStemmer
+
+import numpy
+import re
+import intra.utils as utils
+import requests
+
+
+class Text(object):
+
+
+    @classmethod
+    def from_file(cls, path):
+
+        """
+        Create a text from a filepath.
+
+        :param cls: The Text class.
+        :param path: The filepath.
+        """
+
+        return cls(open(path, 'r').read())
+
+
+    @classmethod
+    def from_url(cls, url):
+
+        """
+        Create a text from a URL.
+
+        :param cls: The Text class.
+        :param path: The URL.
+        """
+
+        return cls(requests.get(url).text)
+
+
+    def __init__(self, text):
+
+        """
+        Store and tokenize the text.
+
+        :param text: The text as a raw string.
+        """
+
+        self.text = text
+        self.tokenize()
+
+
+    def tokenize(self):
+
+        """
+        Tokenize the text.
+        """
+
+        self.tokens = []
+
+        # Strip tags and downcase.
+        text = utils.strip_tags(self.text).lower()
+
+        pattern = re.compile('[a-z]+')
+        porter = PorterStemmer()
+
+        # Index a list of stemmed tokens.
+        for match in re.finditer(pattern, text):
+            self.tokens.append(porter.stem(match.group(0)))
