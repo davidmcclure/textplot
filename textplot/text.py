e9aab2e24cb377a1194283b9f9fb7b14a59152f4
David McClure <davidwilliammcclure@gmail.com>
2014-07-11 15:30:55 -0700

    Store the original token.

--- textplot/text.py
+++ textplot/text.py
@@ -70,12 +70,12 @@ class Text(object):
         for i, match in enumerate(re.finditer(pattern, text)):
 
             # Stem the token.
-            token = match.group(0)
-            stemmed = self.stem(token)
+            original = match.group(0)
+            stemmed = self.stem(original)
 
             # Token:
             self.tokens.append({
-                'token':    token,
+                'original': original,
                 'stemmed':  stemmed,
                 'left':     match.start(),
                 'right':    match.end()
@@ -84,61 +84,3 @@ class Text(object):
             # Token -> offset:
             if stemmed in self.terms: self.terms[stemmed].append(i)
             else: self.terms[stemmed] = [i]
-
-
-    def get_count_difference(self, word1, word2):
-
-        """
-        What is the difference between the number of instances of two words?
-
-        :param word1: The first word.
-        :param word2: The other word.
-        """
-
-        return abs(len(self.terms[word1]) - len(self.terms[word2]))
-
-
-    def get_similarity(self, anchor, sample):
-
-        """
-        Given an "anchor" word, compute a similarity metric for another word.
-
-        :param anchor: The word to compare against.
-        :param sample: The word being compared.
-        """
-
-        distances = []
-
-        anchor_offsets = np.array(self.terms[anchor])
-        sample_offsets = np.array(self.terms[sample])
-
-        for offset in anchor_offsets:
-
-            # Get the closest instance of the sample.
-            cidx = np.abs(sample_offsets - offset).argmin()
-
-            # Get the distance between the two.
-            distance = abs(sample_offsets[cidx] - offset)
-            distances.append(distance)
-
-        return np.mean(np.array(distances))
-
-
-    def get_similarities(self, anchor):
-
-        """
-        Given an "anchor" word, compute a sorted list of similarities for all
-        other terms in the text.
-
-        :param anchor: The word to compare against.
-        """
-
-        sims = []
-        anchor = self.stem(anchor)
-
-        for term in self.terms:
-            if self.get_count_difference(anchor, term) < 500:
-                sims.append((term, self.get_similarity(term, anchor)))
-
-        sims.sort(key=lambda s: s[1])
-        return sims
