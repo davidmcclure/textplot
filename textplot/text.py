98f916e451360d446732de7c066871da9d903e0e
David McClure <davidwilliammcclure@gmail.com>
2014-07-14 12:52:31 -0700

    Notes.

--- textplot/text.py
+++ textplot/text.py
@@ -140,12 +140,11 @@ class Text(object):
 
         anchor = self.stem(anchor)
 
-        sims = []
+        sims = OrderedDict()
         for term in self.terms:
             sim = self.get_pair_similarity(anchor, term, **kwargs)
-            sims.append((term, sim))
+            sims[term] = sim
 
-        sims.sort(key=lambda s: s[1], reverse=True)
         return sims
 
 
@@ -198,3 +197,39 @@ class Text(object):
         result = self.query(query, **kwargs)
         plt.plot(result)
         plt.show()
+
+
+    def plot_2d_term_comparison(self, word1, word2, **kwargs):
+
+        """
+        For two words, compute the similarities with all other words in the
+        text. Then, put word1 on the X axis and word2 on the Y axis, and plot
+        all of the terms according to their weights.
+
+        :param word1: The first word.
+        :param word1: The second word.
+        """
+
+        w1_sims = self.get_all_similarities(word1, **kwargs)
+        w2_sims = self.get_all_similarities(word2, **kwargs)
+
+        xs = [s[1] for s in w1_sims.items()]
+        ys = [s[1] for s in w2_sims.items()]
+        plt.scatter(xs, ys)
+
+        for term in self.terms:
+            plt.annotate(term, (w1_sims[term], w2_sims[term]))
+
+        plt.show()
+
+
+    def plot_weighted_centroids(self, anchors, **kwargs):
+
+        """
+        Given a list of anchor words, plot the weighted centroids of all other
+        terms in the text.
+
+        :param anchors: 3-tuples of (term, X, Y).
+        """
+
+        pass # TODO
