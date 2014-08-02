c9448475f24e323cb56221f525c471f9afd3e00f
David McClure <davidwilliammcclure@gmail.com>
2014-08-01 18:48:11 -0700

    Gets rid of graph building utilities in the text class.

--- textplot/text.py
+++ textplot/text.py
@@ -96,20 +96,6 @@ class Text(object):
         self.filter_terms();
 
 
-    def filter_terms(self, min=5):
-
-        """
-        Set a list of terms that appear more than X times.
-
-        :param min: The minimum number of occurrences.
-        """
-
-        self.terms = set()
-
-        for term, count in self.term_counts().items():
-            if count >= min: self.terms.add(term)
-
-
     def term_counts(self, sort=True):
 
         """
@@ -168,6 +154,23 @@ class Text(object):
         return np.exp(scores) * (len(self.tokens) / samples)
 
 
+    def distance_between_terms(self, term1, term2, **kwargs):
+
+        """
+        How much do the kernel density estimates of two terms overlap?
+
+        :param term1: The first term.
+        :param term2: The second term.
+        """
+
+        t1_kde = self.kde(term1, **kwargs)
+        t2_kde = self.kde(term2, **kwargs)
+
+        # Integrate the overlap.
+        overlap = np.minimum(t1_kde, t2_kde)
+        return np.trapz(overlap)
+
+
     def query(self, query, **kwargs):
 
         """
@@ -217,152 +220,3 @@ class Text(object):
         result = self.query(query, **kwargs)
         plt.plot(result)
         plt.show()
-
-
-    def distance_between_terms(self, term1, term2, **kwargs):
-
-        """
-        How much do the kernel density estimates of two terms overlap?
-
-        :param term1: The first term.
-        :param term2: The second term.
-        """
-
-        t1_kde = self.kde(term1, **kwargs)
-        t2_kde = self.kde(term2, **kwargs)
-
-        # Integrate the overlap.
-        overlap = np.minimum(t1_kde, t2_kde)
-        return np.trapz(overlap)
-
-
-    def index_distances(self, **kwargs):
-
-        """
-        Index all word pair distances.
-        """
-
-        self.distances = {}
-
-        pairs = comb(len(self.terms), 2)
-        with progress.Bar(expected_size=pairs) as bar:
-
-            i = 0
-            for t1, t2 in combinations(self.terms, 2):
-
-                self.set_distance(t1, t2, **kwargs)
-
-                # Update progress.
-                i += 1
-                if i % 1000 == 0: bar.show(i)
-
-
-    def distance_key(self, term1, term2):
-
-        """
-        Get a unique key for a term pair.
-
-        :param term1: The first term.
-        :param term2: The second term.
-        """
-
-        return '_'.join(sorted((term1, term2)))
-
-
-    def set_distance(self, term1, term2, **kwargs):
-
-        """
-        Set the value for a pair of terms.
-
-        :param term1: The first term.
-        :param term2: The second term.
-        """
-
-        k = self.distance_key(term1, term2)
-        d = self.distance_between_terms(term1, term2, **kwargs)
-        self.distances[k] = d
-
-
-    def distance(self, term1, term2):
-
-        """
-        Get the value for a pair of terms.
-
-        :param term1: The first term.
-        :param term2: The second term.
-        """
-
-        k = self.distance_key(term1, term2)
-        if k in self.distances: return self.distances[k]
-        else: return None
-
-
-    def save_distances(self, path):
-
-        """
-        Save the distances as JSON.
-
-        :param path: The file path.
-        """
-
-        with open(path, 'w') as out:
-            json.dump(self.distances, out)
-
-
-    def load_distances(self, path):
-
-        """
-        Load saved term distances JSON.
-
-        :param path: The file path.
-        """
-
-        self.distances = json.load(open(path, 'r'))
-
-
-    def all_distances_from_term(self, anchor, sort=True):
-
-        """
-        Given a term, get the distances for all other words.
-
-        :param anchor: The anchor term.
-        :param sort: If true, sort the dictionary by value.
-        """
-
-        distances = OrderedDict()
-        for term in self.terms:
-            distances[term] = self.distance(anchor, term)
-
-        if sort: distances = utils.sort_dict(distances)
-        return distances
-
-
-    def graph(self, depth=20):
-
-        """
-        For each term, use the liks with the nearest X terms as graph edges.
-
-        :param depth: The number of edges to skim.
-        """
-
-        graph = nx.Graph()
-
-        with progress.Bar(expected_size=len(self.terms)) as bar:
-
-            for i, anchor in enumerate(self.terms):
-
-                # Skim off the top X distances.
-                distances = self.all_distances_from_term(anchor).items()
-
-                for term, distance in distances[:depth]:
-
-                    # Unstem the nodes.
-                    n1 = self.unstem(anchor)
-                    n2 = self.unstem(term)
-
-                    # Register the edge.
-                    graph.add_edge(n1, n2, weight=distance)
-
-                bar.show(i)
-
-        return graph
