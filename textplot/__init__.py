cf40b59d4c75c622b7e26c8cc567353174050954
David McClure <davidwilliammcclure@gmail.com>
2014-10-05 11:49:29 -0700

    Adds a 'spiky' network builder function.

--- textplot/__init__.py
+++ textplot/__init__.py
@@ -1,5 +1,8 @@
 
 
+import utils
+
+from collections import OrderedDict
 from text import Text
 from matrix import Matrix
 from graphs import *
@@ -28,7 +31,7 @@ def frequent(path, term_depth=500, skim_depth=10, d_weights=False, **kwargs):
 def clumpy(path, term_depth=500, skim_depth=10, d_weights=False, **kwargs):
 
     """
-    Use clumpiest terms.
+    Use "clumpiest" terms.
     """
 
     t = Text.from_file(path)
@@ -36,6 +39,40 @@ def clumpy(path, term_depth=500, skim_depth=10, d_weights=False, **kwargs):
 
     print 'Indexing terms:'
     m.index(t.densities(**kwargs).keys()[:term_depth], **kwargs)
+
+    g = Skimmer()
+
+    print 'Generating graph:'
+    g.build(m, skim_depth, d_weights)
+
+    return g
+
+
+def spiky(path, term_depth=5000, spike_depth=1000, skim_depth=10,
+          spike_bw=300000, d_weights=False, **kwargs):
+
+    """
+    Use "spiky-est" terms.
+    """
+
+    t = Text.from_file(path)
+    m = Matrix(t)
+
+    # Get the top X most frequent terms.
+    frequent = t.most_frequent_terms(term_depth)
+
+    # Get the KDE maxes for all terms.
+    kde_maxima = t.normalized_kde_maxima(bandwidth=spike_bw)
+
+    spiky = OrderedDict()
+    for term in frequent:
+        spiky[term] = kde_maxima[term]
+
+    # Sort by KDE max.
+    spiky = utils.sort_dict(spiky)
+
+    print 'Indexing terms:'
+    m.index(spiky.keys()[:spike_depth], **kwargs)
 
     g = Skimmer()
 
