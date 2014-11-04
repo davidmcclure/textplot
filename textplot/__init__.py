9b52ea54c162722d43328407bcbc90620ab9a4d8
David McClure <davidwilliammcclure@gmail.com>
2014-11-04 13:22:28 -0800

    Gets rid of Diachronic graph type, which is now implemented in the Humanist source.

--- textplot/__init__.py
+++ textplot/__init__.py
@@ -46,34 +46,3 @@ def clumpy(path, term_depth=500, skim_depth=10, d_weights=False, **kwargs):
     g.build(m, skim_depth, d_weights)
 
     return g
-
-
-def diachronic(path, term_depth=3000, spike_depth=1000, skim_depth=30,
-             spike_bandwidth=500000, **kwargs):
-
-    """
-    Use most "spiky" terms, to capture diachronic shifts.
-    """
-
-    t = Text.from_file(path)
-    m = Matrix(t)
-
-    # Get the top X most frequent terms.
-    frequent = t.most_frequent_terms(term_depth)
-
-    spiky = OrderedDict()
-    for term in frequent:
-        spiky[term] = t.kde_max(term, bandwidth=spike_bandwidth)
-
-    # Sort by KDE max.
-    spiky = utils.sort_dict(spiky)
-
-    print 'Indexing terms:'
-    m.index(spiky.keys()[:spike_depth], **kwargs)
-
-    g = Diachronic()
-
-    print 'Generating graph:'
-    g.build(m, skim_depth)
-
-    return g
