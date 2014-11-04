9e3cf181539039551bb5d4bb4e5cecb8620d8cac
David McClure <davidwilliammcclure@gmail.com>
2014-11-04 10:40:18 -0800

    Adds setup scripts.

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='textplot',
+    version='0.1.0',
+    description='(Mental) maps of texts.',
+    url='https://github.com/davidmcclure/textplot',
+    license='MIT',
+    author='David McClure',
+    author_email='davidwilliammcclure@gmail.com',
+    include_package_data=True,
+    packages=find_packages(),
+)
