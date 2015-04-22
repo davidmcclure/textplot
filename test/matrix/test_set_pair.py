46d5a2b469f5d5079186471dd6f263b60d841b51
David McClure <davidwilliammcclure@gmail.com>
2015-04-22 16:12:43 -0700

    Test Matrix set/get_pair().

--- test/matrix/test_set_pair.py
+++ test/matrix/test_set_pair.py
@@ -6,10 +6,11 @@ from textplot.matrix import Matrix
 def test_set_pair():
 
     """
-    Matrix#set_pair() should set the value under a key that will always be the
-    same regardless of the order in which the terms are passed.
+    Matrix#set_pair() should set the value under an order-independent key.
     """
 
     m = Matrix()
-    m.set_pair('a', 'b', 5)
-    # TODO
+    m.set_pair('a', 'b', 1)
+
+    assert m.get_pair('a', 'b') == 1
+    assert m.get_pair('b', 'a') == 1
