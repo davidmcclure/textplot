49d9d82493be6e36574e3b60cae46c3660776838
David McClure <davidwilliammcclure@gmail.com>
2015-04-27 14:56:20 -0700

    Tests Text#term_counts().

---
+++ test/text/test_term_counts.py
@@ -0,0 +1,19 @@
+
+
+from textplot.text import Text
+from collections import OrderedDict
+
+
+def test_term_counts():
+
+    """
+    Text#term_counts() should return a map of term -> count.
+    """
+
+    t = Text('aa bb bb cc cc cc')
+
+    assert t.term_counts() == OrderedDict([
+        ('cc', 3),
+        ('bb', 2),
+        ('aa', 1)
+    ])
