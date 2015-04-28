3fcc93843fe08b5c3de8d2307816e9c2a6e0fffa
David McClure <davidwilliammcclure@gmail.com>
2015-04-28 11:03:27 -0700

    Adds tests for TextMatrix#index().

---
+++ test/matrix/test_index.py
@@ -0,0 +1,39 @@
+
+
+from textplot.text import Text
+from textplot.matrix import TextMatrix
+
+
+def test_index():
+
+    """
+    TextMatrix#index() should index the Bray-Curtis distances between terms.
+    """
+
+    t = Text('aa bb cc')
+    m = TextMatrix()
+
+    m.index(t)
+
+    assert m.get_pair('aa', 'bb') == t.score_braycurtis('aa', 'bb')
+    assert m.get_pair('aa', 'cc') == t.score_braycurtis('aa', 'cc')
+    assert m.get_pair('bb', 'cc') == t.score_braycurtis('bb', 'cc')
+
+
+def test_term_subset():
+
+    """
+    When a subset of terms is passed, just those terms should be indexed.
+    """
+
+    t = Text('aa bb cc')
+    m = TextMatrix()
+
+    m.index(t, terms=['aa', 'bb'])
+
+    # Should index 'aa' and 'bb'.
+    assert m.get_pair('aa', 'bb') == t.score_braycurtis('aa', 'bb')
+
+    # Should ignore 'cc'.
+    assert not m.get_pair('aa', 'cc')
+    assert not m.get_pair('bb', 'cc')
