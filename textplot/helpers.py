fa41e2fdde5a17ff64122d19aba877d112a4e96a
David McClure <davidwilliammcclure@gmail.com>
2015-04-29 19:58:03 -0700

    Moves the graph construction helper into a separate helpers module.

---
+++ textplot/helpers.py
@@ -0,0 +1,43 @@
+
+
+import click
+
+from textplot.text import Text
+from textplot.graphs import Skimmer
+from textplot.matrix import Matrix
+
+
+def build_graph(path, term_depth=500, skim_depth=10,
+                d_weights=False, **kwargs):
+
+    """
+    Tokenize a text, index a term matrix, and build out a graph.
+
+    Args:
+        path (str): The file path.
+        term_depth (int): Consider the N most frequent terms.
+        skim_depth (int): Connect each word to the N closest siblings.
+        d_weights (bool): If true, give "close" nodes low weights.
+
+    Returns:
+        Skimmer: The indexed graph.
+    """
+
+    # Tokenize text.
+    click.echo('Tokenizing text...')
+    t = Text.from_file(path)
+    click.echo('Extracted %d tokens' % len(t.tokens))
+
+    m = Matrix()
+
+    # Index the term matrix.
+    click.echo('Indexing terms:')
+    m.index(t, t.most_frequent_terms(term_depth), **kwargs)
+
+    g = Skimmer()
+
+    # Construct the network.
+    click.echo('Generating graph:')
+    g.build(t, m, skim_depth, d_weights)
+
+    return g
