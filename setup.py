ff605ed64a0d932fabdd2e3e0988b1c010797cb9
David McClure <davidwilliammcclure@gmail.com>
2015-05-03 11:46:04 -0700

    Switches license to MIT.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='0.1.0',
     description='(Mental) maps of texts.',
     url='https://github.com/davidmcclure/textplot',
-    license='Apache',
+    license='MIT',
     author='David McClure',
     author_email='davidwilliammcclure@gmail.com',
     include_package_data=True,
